c6b33bcfe5ee30324ea6e9e3fc608679ad68f19d
BuildTools <unconfigured@null.spigotmc.org>
2025-03-14 16:21:28 -0400

    Added event creation form

--- public/create_event.php
+++ public/create_event.php
@@ -1,6 +1,6 @@
 
-#Form to input event title, description, location, date/time.
-#PHP code to insert a new event into events table, referencing the logged-in user (user_id).
+<!-- Form to input event title, description, location, date/time. -->
+<!-- PHP code to insert a new event into events table, referencing the logged-in user (user_id). -->
 
 <?php
 session_start();
@@ -13,7 +13,8 @@ $user_id = $_SESSION["user_id"] ?? 1;
     <meta charset="UTF-8>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Event</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="../assets/css/styles.css">
+    <script src="../assets/js/events.js"></script>
 </head>
 
 <body>
@@ -21,8 +22,17 @@ $user_id = $_SESSION["user_id"] ?? 1;
     <form action="create_event.php" method="POST" id="eventForm">
         <input type="hidden" name="user_id" value="1">
         <input type="text" name="title" placeholder="Event Title" required>
+        <textarea name="description" placeholder="Event Description"></textarea>
+        <!-- how specific do we want to get with location? address? -->
+        <input type="text" name="location" placeholder="Location">
+        <input type="datetime-local" name="event_date" required>
+        <button type="submit">Create Event</button>
 
     </form>
+
+    <div id="statusmessage"></div> <!-- event creation success/error message -->
+
+    <script src="../assets/js/event.js"></script>
 </body>
 
 </html>
