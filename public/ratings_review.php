060c00bc7827a7763ca0c947d89d11b5606c7ba3
zdaula <163958719+zdaula@users.noreply.github.com>
2025-03-15 15:21:27 -0400

    Update ratings_review.php

--- public/ratings_review.php
+++ public/ratings_review.php
@@ -5,4 +5,11 @@
       <Meta name="viewport", content="width=device-width, height="device-height", initial-scale="1.0">
     <title> uOttawa NightLife </title>
   </head>
-  
+  <style>
+    body {
+        font-family: 'Poppins', sans-serif; 
+        margin: 0;
+        padding: 0;
+        background-color: #E6F0FA;
+        color: #222;
+    }
