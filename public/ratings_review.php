9dc065c9eae310d9b9695afd13d2a5d1c8ecc48e
joshua-connelly <jconn007@uottawa.ca>
2025-03-15 15:50:27 -0400

    Update ratings_review.php

--- public/ratings_review.php
+++ public/ratings_review.php
@@ -4,12 +4,28 @@
     <Meta charset="UTF-8"
       <Meta name="viewport", content="width=device-width, height="device-height", initial-scale="1.0">
     <title> uOttawa NightLife </title>
-  </head>
   <style>
-    body {
+body {
         font-family: 'Poppins', sans-serif; 
         margin: 0;
         padding: 0;
         background-color: #E6F0FA;
         color: #222;
     }
+    
+header {
+        background-color: #09223b; 
+        color: #cbd1d6;
+        padding: 10px 0;
+        text-align: center;
+        }
+    
+footer {
+        text-align: center;
+        background-color: #aaacad
+        color: #494f52
+        padding: 10px
+}
+    
+    </style>
+  </head>
