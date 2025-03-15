97ff804843f0ab2d3e9d0f2a4fac424d7892e44a
joshua-connelly <jconn007@uottawa.ca>
2025-03-15 15:14:43 -0400

    Update ratings_review.php

--- public/ratings_review.php
+++ public/ratings_review.php
@@ -1,1 +1,8 @@
-<!DOCTYPE
+<!DOCTYPE html> 
+<html lang="en">
+  <head>
+    <Meta charset="UTF-8"
+      <Meta name="viewport", content="width=device-width, height="device-height", initial-scale="1.0">
+    <title> uOttawa NightLife </title>
+  </head>
+  
