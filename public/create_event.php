fcf0fd7625a8265d83a7aea483325a68098a5329
BuildTools <unconfigured@null.spigotmc.org>
2025-03-16 14:04:51 -0400

    Made front end of event creation. Added assets folder for CSS and JS.

--- public/create_event.php
+++ public/create_event.php
@@ -14,17 +14,15 @@ $user_id = $_SESSION["user_id"] ?? 1;
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Create Event</title>
     <link rel="stylesheet" href="../assets/css/styles.css">
-    <script src="../assets/js/events.js"></script>
 </head>
 
 <body>
     <h2>Create a New Event!</h2>
-    <form action="create_event.php" method="POST" id="eventForm">
-        <input type="hidden" name="user_id" value="1">
+    <form id="eventForm">
+        <input type="hidden" name="user_id" value="1"> <!-- replace the 1 here later with session user ID -->
         <input type="text" name="title" placeholder="Event Title" required>
         <textarea name="description" placeholder="Event Description"></textarea>
-        <!-- how specific do we want to get with location? address? -->
-        <input type="text" name="location" placeholder="Location">
+        <input type="text" name="location" placeholder="Enter Address">
         <input type="datetime-local" name="event_date" required>
         <button type="submit">Create Event</button>
 
