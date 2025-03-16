7b26c54b635916fca829e292ab9d443f61926534
joshua-connelly <jconn007@uottawa.ca>
2025-03-16 15:22:22 -0400

    Update ratings_review.php

--- public/ratings_review.php
+++ public/ratings_review.php
@@ -46,8 +46,59 @@ footer {
 
 <div class="review">
     <h1>Happy Fish</h1>
-    <p>Rating: ★★★☆☆</p>
-    <p><b>Dinner, After-hours, Drinks</b></p>
-    <p>"Cozy place with a friendly vibe. The menu is diverse, though some dishes are hit or miss. Great for a casual night out."</p>
+    <p>Rating: ★★★★☆</p>
+    <p><b>Outdoor seating, Great cocktails, Dancing</b></p>
+    <p>"Great spot but way too full for the size of the place most times"</p>
     <p>Price: $$ - $$$</p>
 </div>
+
+
+<div class="review">
+    <h1>Heart & Crown</h1>
+    <p>Rating: ★★★★☆</p>
+    <p><b>Outdoor seating, Great cocktails, Fireplace</b></p>
+    <p>"Enjoyed the live music here on a Sunday. Prices are a bit high for a pub, but the cocktails we ordered all tasted great."</p>
+    <p>Price: $$ - $$$</p>
+</div>
+
+
+<div class="review">
+    <h1>Lieutenant's Pump</h1>
+    <p>Rating: ★★★★☆</p>
+    <p><b>Outdoor seating, Great cocktails, Trivia Night</b></p>
+    <p>"Classic Brit-Canadian pub. It's much larger than it looks from the outside. Very dark inside but friendly atmosphere. Good pub fare and great pints."</p>
+    <p>Price: $$ - $$$</p>
+</div>
+
+  <div class="review">
+    <h1>El Furniture warehouse</h1>
+    <p>Rating: ★★★★☆</p>
+    <p><b>Happy hour food, Great cocktails, Doesn't accept reservations</b></p>
+    <p>"Nice place to have some drinks and hang out. Prices are decent. Only negative was the Food was alright at best."</p>
+    <p>Price: $ </p>
+</div>
+
+    <div class="review">
+    <h1>The Nelson Pub & Eatery</h1>
+    <p>Rating: ★★★★★</p>
+    <p><b>Great cocktails, Good for watching sports, Wi-Fi</b></p>
+    <p>"I love this place. The service is great, menu is affordable, and the drinks are delicious. Food is solid too!"</p>
+    <p>Price: $$</p>
+</div>
+
+      <div class="review">
+    <h1>Pub 101</h1>
+    <p>Rating: ★★★★☆</p>
+    <p><b>Easygoing bar, TVs, Patio, Karaoke</b></p>
+    <p>"The prices are excellent, and portions are perfect, not giant, not too small. The atmosphere is excellent with nice live music on some nights. It's 3 floors and fun."</p>
+    <p>Price: $$</p>
+</div>
+
+        <div class="review">
+    <h1>Lowertown Brewery, Byward Market</h1>
+    <p>Rating: ★★★★☆</p>
+    <p><b>Outdoor seating, Great cocktails, Vegetarian Options</b></p>
+    <p>"Had a blast at Lowertown! Went with some friends for drinks and a hot pretzel. Fun atmosphere."</p>
+    <p>Price: $$ - $$$</p>
+</div>
+  
