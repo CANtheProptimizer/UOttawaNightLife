5c306990acb6dca281bf7c8836d7390f081ebbf3
joshua-connelly <jconn007@uottawa.ca>
2025-03-16 19:18:44 -0400

    Update ratings_review.php

--- public/ratings_review.php
+++ public/ratings_review.php
@@ -45,101 +45,101 @@ footer {
 
 
 <div class="review">
-    <h1>Happy Fish</h1>
+    <h1><b>Happy Fish</b></h1>
     <p>Rating: ★★★★☆</p>
-    <p><b>Outdoor seating, Great cocktails, Dancing</b></p>
+    <p>Outdoor seating, Great cocktails, Dancing</p>
     <p>"Great spot but way too full for the size of the place most times"</p>
     <p>Price: $$ - $$$</p>
 </div>
 
 
 <div class="review">
-    <h1>Heart & Crown</h1>
+    <h1><b>Heart & Crown</b></h1>
     <p>Rating: ★★★★☆</p>
-    <p><b>Outdoor seating, Great cocktails, Fireplace</b></p>
+    <p>Outdoor seating, Great cocktails, Fireplace</p>
     <p>"Enjoyed the live music here on a Sunday. Prices are a bit high for a pub, but the cocktails we ordered all tasted great."</p>
     <p>Price: $$ - $$$</p>
 </div>
 
 
 <div class="review">
-    <h1>Lieutenant's Pump</h1>
+    <h1><b>Lieutenant's Pump</b></h1>
     <p>Rating: ★★★★☆</p>
-    <p><b>Outdoor seating, Great cocktails, Trivia Night</b></p>
+    <p>Outdoor seating, Great cocktails, Trivia Night</p>
     <p>"Classic Brit-Canadian pub. It's much larger than it looks from the outside. Very dark inside but friendly atmosphere. Good pub fare and great pints."</p>
     <p>Price: $$ - $$$</p>
 </div>
 
   <div class="review">
-    <h1>El Furniture warehouse</h1>
+    <h1><b>El Furniture warehouse</b></h1>
     <p>Rating: ★★★★☆</p>
-    <p><b>Happy hour food, Great cocktails, Doesn't accept reservations</b></p>
+    <p>Happy hour food, Great cocktails, Doesn't accept reservations</p>
     <p>"Nice place to have some drinks and hang out. Prices are decent. Only negative was the Food was alright at best."</p>
     <p>Price: $ </p>
 </div>
 
     <div class="review">
-    <h1>The Nelson Pub & Eatery</h1>
+    <h1><b>The Nelson Pub & Eatery</b></h1>
     <p>Rating: ★★★★★</p>
-    <p><b>Great cocktails, Good for watching sports, Wi-Fi</b></p>
+    <p>Great cocktails, Good for watching sports, Wi-Fi</p>
     <p>"I love this place. The service is great, menu is affordable, and the drinks are delicious. Food is solid too!"</p>
     <p>Price: $$</p>
 </div>
 
       <div class="review">
-    <h1>Pub 101</h1>
+    <h1><b>Pub 101</b></h1>
     <p>Rating: ★★★★☆</p>
-    <p><b>Easygoing bar, TVs, Patio, Karaoke</b></p>
+    <p>Easygoing bar, TVs, Patio, Karaoke</p>
     <p>"The prices are excellent, and portions are perfect, not giant, not too small. The atmosphere is excellent with nice live music on some nights. It's 3 floors and fun."</p>
     <p>Price: $$</p>
 </div>
 
         <div class="review">
-    <h1>Lowertown Brewery, Byward Market</h1>
+    <h1><b>Lowertown Brewery, Byward Market</b></h1>
     <p>Rating: ★★★★☆</p>
-    <p><b>Outdoor seating, Great cocktails, Vegetarian Options</b></p>
+    <p>Outdoor seating, Great cocktails, Vegetarian Options</p>
     <p>"Had a blast at Lowertown! Went with some friends for drinks and a hot pretzel. Fun atmosphere."</p>
     <p>Price: $$ - $$$</p>
 </div>
 
           <div class="review">
-    <h1>The Senate Tavern on Clarencet</h1>
+    <h1><b>The Senate Tavern on Clarencet</b></h1>
     <p>Rating: ★★★★☆</p>
-    <p><b>Cozy, Sports-themed, Canadian pub grub & draft beers</b></p>
+    <p>Cozy, Sports-themed, Canadian pub grub & draft beers</p>
     <p>"Great beer selection. Love the craft. Beer amazing. Washroom very clean."</p>
     <p>Price: $$ - $$$</p>
 </div>
 
           <div class="review">
-    <h1>Level One Game Pub</h1>
+    <h1><b>Level One Game Pub</b></h1>
     <p>Rating: ★★★★★</p>
-    <p><b>Great cocktails, Vegan options, Live music</b></p>
+    <p>Great cocktails, Vegan options, Live music</p>
     <p>"The atmosphere here is awesome! This place has a great selection of board games and video games too! The staff here are very friendly and the drinks and food are super tasty. Lots of space in the venue as well for activities.
 "</p>
     <p>Price: $$ - $$$</p>
 </div>
   
           <div class="review">
-    <h1>House of TARG</h1>
+    <h1><b>House of TARG</b></h1>
     <p>Rating: ★★★★★</p>
-    <p><b>Great cocktails, Live performances, Dancing</b></p>
+    <p>Great cocktails, Live performances, Dancing</p>
     <p>"Targ is always a great time. The atmosphere is lively and casual. If you go early on the free-play days, there are lots of machines available, although it gets busy as the day goes on. The staff is always friendly and helpful. The pinball machines are well maintained and there is a great variety of games from all different points in time. Don't forget to try their famous pierogies, which never disappoint!".</p>
     <p>Price: $$ - $$$</p>
 </div>
 
           <div class="review">
-    <h1>Hangout</h1>
+    <h1><b>Hangout</b></h1>
     <p>Rating: ★★★★☆</p>
-    <p><b>Bubble Tea, Shaved Ice, & Brick Toasts with board and arcade games</b></p>
+    <p>Bubble Tea, Shaved Ice, & Brick Toasts with board and arcade games</p>
     <p>"This is a really interesting, and AFFORDABLE, place to hang out. I love the intersection of niches that are found here:  boardgames, ramen, and bumble tea :)"</p>
     <p>Price: $</p>
 </div>
 
 
           <div class="review">
-    <h1>The Gilmour</h1>
+    <h1><b>The Gilmour</b></h1>
     <p>Rating: ★★★★★</p>
-    <p><b>Outdoor seating, Great cocktails, Live music</b></p>
+    <p>Outdoor seating, Great cocktails, Live music</p>
     <p>"Good for bar food, decent drink options. Lots of board games. Fun 80s/90s Playlist."</p>
     <p>Price: $$</p>
 </div>  
